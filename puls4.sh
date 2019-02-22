9ac3ed691048088d2b207bbd9a29de0c67a85343
izakardoni <46862073+izakardoni@users.noreply.github.com>
2019-02-23 02:22:57 +0700

    Add files via upload

---
+++ puls4.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+figlet PULSA.PHP |lolcat
+
+echo "{×}==================================={×}"|lolcat
+echo "Creator: Mr.ECA TRIANGLE IZ4K412"|lolcat
+echo "-----------------------------------------"|lolcat
+echo "Team   : 1.ELANG CYBER ARMY"|lolcat
+echo "         2.FAMILY QUICK TRIANGLE"|lolcat
+echo "-----------------------------------------"|lolcat
+echo "Thanks : > SK4T3R AND TEAM"|lolcat
+echo "         > The Coward"|lolcat
+echo "         > IPULL=TUAN SEMPAK=TUAN G4L4U"|lolcat
+echo "{×}==================================={×}"|lolcat
+echo ""   
+echo "Oke cuk ga usah banyak bacot, langsung pilih mau apa"|lolcat
+echo "___________________________________________________"|lolcat
+echo ""
+echo "1.> ISI PULSA SENDIRI    "$red
+echo ""
+echo "2.> ISI PULSA TEMAN      "$green
+echo "___________________________________________________"|lolcat
+
+echo ""
+echo "\033[34;1m  
+99\033[37;1m.\033[32;1mKeluar\033[37;1m"
+echo ""
+echo "\033[32;1m ──> \033[32;1m Pilih pilih aja gk usah.komen!\033[32;1m"
+read -p " PILIHAN=> " pilih
+if [ $pilih = 1 ]
+then
+clear
+figlet ISI PULSA | lolcat
+echo "\033[34;1msabar asw"
+echo ""
+sleep 1
+
+echo "mau isi pulsa berapa.?"
+read -p "(ex:5000, 10000 dan seterusnya)=> " pulsa
+echo ""
+echo ""
+echo "nomor lu"
+read -p "nomor pake +62 eaa =>" nomor
+echo "Sabar asw lagi ngirim eaa"  
+echo ""
+echo ""
+echo "jumlah pulsa :" $pulsa|lolcat
+echo "ke nomer :" $nomor |lolcat
+echo ""
+echo ""
+sleep 2
+clear
+figlet LOADING|lolcat
+sleep 15
+echo ""
+echo "Wkwkwkwkwkwkwkwkwkwkwkwkwkwkw"|lolcat
+echo ""
+sleep 2
+echo "mau aja lu gw kerjain. makan tuh pulsa gratis"|lolcat
+echo "makanya modal dong, jgn cuma mau yg gratisan mulu"|lolcat
+echo ""
+sleep 2
+echo "ya uda makasih eaa uda coba tools gua"|lolcat
+echo "pesan gw"|lolcat
+sleep 2
+echo "kalean semua BURIQQ, gw doang yg GANNSS"|lolcat
+echo "By.Mr.ECA TRIANGLE IZ4K412"|lolcat
+sleep 2
+figlet B . Y . E|lolcat
+fi
+
+if [ $pilih = 2 ]
+then
+clear
+figlet SEND PULSA | lolcat
+echo "\033[34;1msabar asw"
+echo ""
+sleep 1
+
+echo "mau isiin pulsa temen kamu eaa"
+echo "mau send pulsa brp.?"
+read -p "(ex:5000, 10000 dan seterusnya)=> " a
+echo ""
+echo ""
+echo "OPERATOR"
+read -p "(ex:Indosat,3 , dll=> " b
+echo ""
+echo ""
+echo "nomor tujuan"
+read -p "=>" c
+echo ""
+echo ""
+sleep 1
+echo "nomor target : " $c|lolcat
+echo "OPERATOR     : " $b|lolcat
+echo "Jumlah pulsa : " $a |lolcat
+sleep 2
+clear
+figlet LOADING|lolcat
+sleep 15
+echo ""
+echo "Wkwkwkwkwkwkwkwkwkwkwkwkwkwkw"|lolcat
+echo ""
+sleep 2
+echo "mau aja lu gw kerjain. makan tuh pulsa gratis"|lolcat
+echo "makanya modal dong, jgn cuma mau yg gratisan mulu"|lolcat
+echo ""
+sleep 2
+echo "ya uda makasih eaa uda coba tools gua"|lolcat
+echo "pesan gw"|lolcat
+sleep 2
+echo "kalean semua BURIQQ, gw doang yg GANNSS"|lolcat
+echo "By.Mr.ECA TRIANGLE IZ4K412"|lolcat
+sleep 2
+figlet B . Y . E|lolcat
+fi
